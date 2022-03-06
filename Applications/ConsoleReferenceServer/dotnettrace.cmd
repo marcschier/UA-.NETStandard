a198f97e71c4b7bc11ce478db7f33f9891fe5660
Marc Schier <marcschier@hotmail.com>
2022-03-06 13:13:37 +0100

    Initial

--- Applications/ConsoleReferenceServer/dotnettrace.cmd
+++
@@ -1,3 +0,0 @@
-REM collect a trace using the EventSource provider OPC-UA-Core
-dotnet tool install --global dotnet-trace
-dotnet-trace collect --name consolereferenceserver --providers OPC-UA-Core,OPC-UA-Server
